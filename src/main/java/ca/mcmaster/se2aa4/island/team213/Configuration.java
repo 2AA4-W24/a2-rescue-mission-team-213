64e0e148d6629c16ee5ebee227e342ea925427ce
Gary Qin <garyzhqin@gmail.com>
2024-02-08 14:45:36 -0500

    test

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team213/Configuration.java
@@ -0,0 +1,10 @@
+package ca.mcmaster.se2aa4.island.team213;
+
+public class Configuration {
+    private String context;
+    
+    public Configuration(String context) {
+        this.context = context;
+    }
+    
+}
