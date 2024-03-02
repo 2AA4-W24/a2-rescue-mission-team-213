7c7e0c2dda13fcd1f1aaf4437e0f8e6b491ed4c4
Gary Qin <garyzhqin@gmail.com>
2024-03-01 19:39:34 -0500

    Update AreaFinder.java phase logic

--- src/main/java/ca/mcmaster/se2aa4/island/team213/AreaFinder.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team213/AreaFinder.java
@@ -1,13 +1,12 @@
 package ca.mcmaster.se2aa4.island.team213;
 
-import org.apache.logging.log4j.core.parser.JsonLogEventParser;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class AreaFinder {
     private int length, width;
 
-    private boolean a, b, c, d, turnRight; // secondary phases
+    private boolean a, b, c, turnRight; // secondary phases
     private boolean movedForward, scanned, echoedLeft, echoedRight; // tertiary phases
 
     private String previousDecision;
@@ -18,8 +17,7 @@ public class AreaFinder {
     // Assume drone starting direction is west
     public AreaFinder() {
         this.length = 0;
-        this.width = 0;
-        this.turnRight = false;
+        this.width = 1;
 
         resetSecondaryPhases();
         resetTertiaryPhases();
@@ -29,7 +27,7 @@ public class AreaFinder {
         this.a = false;
         this.b = false;
         this.c = false;
-        this.d = false;
+        this.turnRight = false;
     }
 
     private void resetTertiaryPhases() {
@@ -40,7 +38,7 @@ public class AreaFinder {
     }
 
     public boolean isFinished() { // primary phase interface method
-        if(a && b && c && d) {
+        if(a && b && c) {
             return true;
         }
         return false;
@@ -50,30 +48,26 @@ public class AreaFinder {
         JSONObject decision = new JSONObject();
 
         if(this.turnRight) {
-            JSONObject parameter = new JSONObject();
+            this.previousDecision = "turnRight";
+            this.movedForward = true;
             this.direction = this.direction.rightTurn();
 
+            JSONObject parameter = new JSONObject();
             parameter.put("direction", this.direction);
             decision.put("parameters", parameter);
             decision.put("action", "heading");
+
             this.turnRight = false;
         }
-        else if(!this.a) 
-        {
+        else if(!this.a) {
             if(!this.movedForward) {
                 this.previousDecision = "fly";
                 this.movedForward = true;
+
+                decision.put("action", previousDecision);
             } else if(!this.scanned) {
                 this.previousDecision = "scan";
                 this.scanned = true;
-            }
-            decision.put("action", this.previousDecision);
-        } 
-        else if(!this.b) 
-        {
-            if(!this.movedForward) {
-                this.previousDecision = "fly";
-                this.movedForward = true;
 
                 decision.put("action", this.previousDecision);
             } else if(!this.echoedLeft) {
@@ -93,6 +87,25 @@ public class AreaFinder {
                 decision.put("parameters", parameter);
                 decision.put("action", "echo");
             }
+        } 
+        else if(!this.b) {
+            if(!this.movedForward) {
+                this.previousDecision = "fly";
+                this.movedForward = true;
+
+                decision.put("action", this.previousDecision);
+            } else if(!this.echoedRight) {
+                this.previousDecision = "echoRight";
+                this.echoedRight = true;
+
+                JSONObject parameter = new JSONObject();
+                parameter.put("direction", this.direction.rightTurn());
+                decision.put("parameters", parameter);
+                decision.put("action", "echo");
+            }
+        }
+        else if(!this.c) {
+            
         }
 
         return decision;
@@ -102,7 +115,6 @@ public class AreaFinder {
         if(previousDecision.equals("scan")) 
         {
             this.scanInfo = response.getJSONObject("extras").getJSONArray("biomes");
-            checkScan();
         }
         else if(previousDecision.equals("echoLeft"))
         {
@@ -111,31 +123,19 @@ public class AreaFinder {
         else if(previousDecision.equals("echoRight"))
         {
             this.rightEcho = response.getJSONObject("extras");
-            checkEchos();
+            checkScanAndEchoes();
         }
     }
 
-    private void checkScan() {
-        if(this.scanInfo.getString(0).equals("BEACH")) {
-            this.a = true;
-        } else {
+    private void checkScanAndEchoes() {
+        if(!a) {
+            if (this.scanInfo.length() == 1 && this.scanInfo.getString(0).equals("BEACH") && this.leftEcho.getString("found").equals("OUT_OF_RANGE") && this.rightEcho.getString("found").equals("OUT_OF_RANGE")) {
+                this.a = true;
+                this.turnRight = true;
+                this.length += 1;
+            } 
             this.length += 1;
             resetTertiaryPhases();
         }
-        
     }
-
-    private void checkEchos() {
-        if(!b) {
-            if(this.leftEcho.getString("found").equals("OUT_OF_RANGE") && this.rightEcho.getString("found").equals("OUT_OF_RANGE")) {
-                this.b = true;
-                this.turnRight = true;
-                this.length += 1;
-            } else {
-                resetTertiaryPhases();
-            }
-            this.length += 1;
-        }
-    }
-
 }
