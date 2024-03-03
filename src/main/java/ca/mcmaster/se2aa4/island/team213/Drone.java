df3a45c2cf1338288b270d268cc181c34ed9b578
Colin Chambachan <chambacc@mcmaster.ca>
2024-03-03 15:58:16 -0500

    refactor: implement updated drone class

--- src/main/java/ca/mcmaster/se2aa4/island/team213/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team213/Drone.java
@@ -11,11 +11,13 @@ public class Drone {
     private Direction direction;
     private EchoStatus echo = new EchoStatus();
     private Direction echoing;
+    private String droneStatus;
     private JSONArray scanInfo;
     private String previousDecision;
 
     private final Logger logger = LogManager.getLogger();
-    
+    private Direction directionHeading;
+
     public Drone(String direction, Integer battery){
         stringToDirection(direction);
         this.battery = battery;
@@ -31,53 +33,60 @@ public class Drone {
     }
 
     public void updateStatus(JSONObject response){
-
+        logger.info("updating status...");
         //        logger.info("** Response received:\n"+response.toString(2));
-        //        logger.info("The cost of the action was {}", cost);
+
         battery -= response.getInt("cost");
-        //        logger.info("The status of the drone is {}", status);
-        //        droneStatus = response.getString("status");
+//                logger.info("New battery: {}", battery);
+        droneStatus = response.getString("status");
+//                logger.info("The status of the drone is {}", droneStatus);
 
 
         // TECHNICAL DEBT: currently just assumes content of extra is echo response object, does not consider for scan
         //        logger.info("Additional information received: {}", extraInfo);
+
         JSONObject extraInfo = response.getJSONObject("extras");
-        String result = extraInfo.getString("found");
-        Integer range = extraInfo.getInt("range");
+        if (!extraInfo.isEmpty()){
+            String result = extraInfo.getString("found");
+            Integer range = extraInfo.getInt("range");
 
-        switch (echoing) {
-            case N -> {
-                echo.north = EchoResult.valueOf(result);
-                echo.rangeNorth = range;
-            }
-            case E -> {
-                echo.east = EchoResult.valueOf(result);
-                echo.rangeEast = range;
+            if (echoing != null){
+                switch (echoing) {
+                    case N -> {
+                        echo.north = EchoResult.valueOf(result);
+                        echo.rangeNorth = range;
+                    }
+                    case E -> {
+                        echo.east = EchoResult.valueOf(result);
+                        echo.rangeEast = range;
 
-            }
-            case S -> {
-                echo.south = EchoResult.valueOf(result);
-                echo.rangeSouth = range;
-            }
-            case W -> {
-                echo.west = EchoResult.valueOf(result);
-                echo.rangeWest = range;
+                    }
+                    case S -> {
+                        echo.south = EchoResult.valueOf(result);
+                        echo.rangeSouth = range;
+                    }
+                    case W -> {
+                        echo.west = EchoResult.valueOf(result);
+                        echo.rangeWest = range;
+                    }
+                }
+//            logger.info(echo.east.toString());
+//            logger.info(echo.rangeEast);
+
+                // set echoing to null after
+                echoing = null;
             }
         }
-        logger.info(echo.east.toString());
-        logger.info(echo.rangeEast);
 
-        // set echoing to null after
-        echoing = null;
+
+
 
     }
 
     public EchoResult getEchoNorth(){
         return echo.north;
     }
-    public EchoResult getEchoEast(){
-        return echo.east;
-    }
+    public EchoResult getEchoEast(){return echo.east;}
     public EchoResult getEchoSouth(){
         return echo.south;
     }
@@ -85,8 +94,64 @@ public class Drone {
         return echo.west;
     }
 
-    public void setEcho(Direction echoing) {
-        this.echoing = echoing;
+    public Integer getRangeHeading(){
+        switch (directionHeading){
+            case N ->{
+                return echo.rangeNorth;
+            }
+            case E ->{
+                return echo.rangeEast;
+            }
+            case S ->{
+                return echo.rangeSouth;
+            }
+            case W ->{
+                return echo.rangeWest;
+            }
+        }
+
+        // Should not arrive at this case
+        return null;
+    }
+    public void setDirectionHeading(Direction newDirection){
+        this.directionHeading = newDirection;
+    }
+    public void subtractRangeHeading(){
+        switch (directionHeading){
+            case N ->{
+                echo.rangeNorth--;
+            }
+            case E ->{
+                echo.rangeEast--;
+            }
+            case S ->{
+                echo.rangeSouth--;
+            }
+            case W ->{
+                echo.rangeWest--;
+            }
+        }
+
+    }
+
+    public EchoResult getEchoHeading(){
+        switch (directionHeading){
+            case N ->{
+                return echo.north;
+            }
+            case E ->{
+                return echo.east;
+            }
+            case S ->{
+                return echo.south;
+            }
+            case W ->{
+                return echo.west;
+            }
+        }
+
+        // Should not arrive at this case
+        return null;
     }
 
 
