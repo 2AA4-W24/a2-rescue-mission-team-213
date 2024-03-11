30a5595b419987a5656f295cf6785a67dc1447d2
Colin Chambachan <chambacc@mcmaster.ca>
2024-03-11 18:05:30 -0400

    bugfix: update JSON error

--- src/main/java/ca/mcmaster/se2aa4/island/team213/Action.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team213/Action.java
@@ -1,9 +1,7 @@
 package ca.mcmaster.se2aa4.island.team213;
 
 public enum Action {
-<<<<<<< Updated upstream
-    TURN_RIGHT, TURN_LEFT, ECHO_RIGHT, ECHO_LEFT, ECHO_AHEAD, SCAN, FLY;
-=======
-    turnRight, turnLeft, echoRight, echoLeft, echoAhead, scan, fly, stop
->>>>>>> Stashed changes
+
+    TURN_RIGHT, TURN_LEFT, ECHO_RIGHT, ECHO_LEFT, ECHO_AHEAD, SCAN, FLY, STOP;
+
 }
